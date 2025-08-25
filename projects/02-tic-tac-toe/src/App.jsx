7b39dd88446d368af974ec03279002b074d93234
AntonioMonzo <amonansu@gmail.com>
2025-08-25 21:31:15 +0200

    UPDATE

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -4,15 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const TURNS = {
-  x: 'x',
-  O: 'o',
+  X: 'X',
+  O: 'O',
 }
 
-const Square = ({children, isSelected, updateBoard, index }) =>{
+const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
 
   return (
@@ -22,16 +22,61 @@ const Square = ({children, isSelected, updateBoard, index }) =>{
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))/*Estado inicial*/
-  
-  const [turn, setTurn] = useState(TURNS.x)
 
-  const updateBoard = () =>{
-    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
-    setTurn(newTurn)
+  const [turn, setTurn] = useState(TURNS.x)
+  const [winner, setWinner] = useState(null)//ESTADO DE GANADOR null no ganador y false empata
+
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a]
+      }
+    }
+    return null
   }
+
+  const updateBoard = (index) => {
+
+    //COMPROBAR POSICIONES
+    if (board[index] || winner) return/*Si ya hay algo en esa posición, no hacer nada*/
+
+    //ACTUALIZAR TABLERO
+    const newBoard = [...board]//todo dentro de un nuevo array 
+    newBoard[index] = turn//asignar el turno actual al nuevo tablero
+    setBoard(newBoard)//actualizar el estado del tablero
+
+    //ACTUALIZAR TURNO
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X//asignar el nuevo turno
+    setTurn(newTurn)//actualizar el estado del turno
+
+    //COMPROBAR GANADOR
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner) //esto es asíncrono esto se debe a que React agrupa las actualizaciones de estado es decir, que puede que no se actualice inmediatamente
+    //Las actualizaciones de estado en React son asíncronas, lo que significa que el nuevo estado puede no estar disponible inmediatamente después de llamar a setState.
+    }
+
+    }
+
+  
 
 
   return (
@@ -42,13 +87,13 @@ function App() {
 
       <section className='game'>
         return (
-          <Square
+        <Square
           key={index}/**Pq es único sino no funcionaría */
           index={index}
           updateBoard={updateBoard}
-          >
-            {index}
-          </Square>
+        >
+          {index}
+        </Square>
         )
       </section>
 
@@ -62,7 +107,7 @@ function App() {
       </section>
 
     </main>
-    )
-
+  )
 }
+
 export default App
